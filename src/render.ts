2412845995b3af929b4462af0ba177e263f33ef3
河田鷹介 <benefium@gmail.com>
2025-02-02 14:17:28 +0900

    fix: ガード追加

--- src/render.ts
+++ src/render.ts
@@ -25,50 +25,48 @@ function renderTSTrieNode(
       }
 
       case 'array': {
-        if (candidate.arrayChildren) {
-          const elementType = renderTSTrieNode(candidate.arrayChildren, indentLevel);
-          types.push(`Array<${elementType}>`);
-        } else {
-          types.push('any[]');
+        if (!candidate.arrayChildren) {
+          throw new Error('Array candidate should have arrayChildren');
         }
+        const elementType = renderTSTrieNode(candidate.arrayChildren, indentLevel);
+        types.push(`Array<${elementType}>`);
         break;
       }
 
       case 'object': {
         const baseCount = candidate.count;
-        if (candidate.objectChildren) {
-          const props: {
-            keyPart: string;
-            typePart: string;
-          }[] = [];
-          const indentSpaces1 = ' '.repeat((indentLevel + 1) * spacesForTab);
-
-          let maxKeyPartLength = 0;
-          for (const prop of Object.keys(candidate.objectChildren).sort()) {
-            // NOTE: 厳密なフィールド所持判定
-            if (Object.prototype.hasOwnProperty.call(candidate.objectChildren, prop)) {
-              const childNode = candidate.objectChildren[prop]!;
-              const childType = renderTSTrieNode(childNode, indentLevel + 1);
-              const childCount = childNode.count;
-              // NOTE: < の場合は何かがおかしい
-              const isRequired = baseCount <= childCount;
-              const keyPart = formatAsObjectKey(prop) + ':' + (isRequired ? '' : '?') 
-              if (maxKeyPartLength < keyPart.length) {
-                maxKeyPartLength = keyPart.length;
-              }
-              props.push({
-                keyPart,
-                typePart: childType,
-              });
-            }
-          }
-
-          types.push(`{\n${props.map(p => {
-            return `${indentSpaces1}${p.keyPart.padEnd(maxKeyPartLength)} ${p.typePart};\n`;
-          }).join("")}${indentSpaces0}}`);
-        } else {
-          types.push('{}');
+        if (!candidate.objectChildren) {
+          throw new Error('Object candidate should have objectChildren');
         }
+        const props: {
+          keyPart: string;
+          typePart: string;
+        }[] = [];
+        const indentSpaces1 = ' '.repeat((indentLevel + 1) * spacesForTab);
+
+        let maxKeyPartLength = 0;
+        for (const prop of Object.keys(candidate.objectChildren).sort()) {
+          // NOTE: 厳密なフィールド所持判定
+          if (Object.prototype.hasOwnProperty.call(candidate.objectChildren, prop)) {
+            const childNode = candidate.objectChildren[prop]!;
+            const childType = renderTSTrieNode(childNode, indentLevel + 1);
+            const childCount = childNode.count;
+            // NOTE: < の場合は何かがおかしい
+            const isRequired = baseCount <= childCount;
+            const keyPart = formatAsObjectKey(prop) + ':' + (isRequired ? '' : '?') 
+            if (maxKeyPartLength < keyPart.length) {
+              maxKeyPartLength = keyPart.length;
+            }
+            props.push({
+              keyPart,
+              typePart: childType,
+            });
+          }
+        }
+
+        types.push(`{\n${props.map(p => {
+          return `${indentSpaces1}${p.keyPart.padEnd(maxKeyPartLength)} ${p.typePart};\n`;
+        }).join("")}${indentSpaces0}}`);
         break;
       }
     }
