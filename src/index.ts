b64ec190d205f16acee368418e1fa44264b943f4
河田鷹介 <benefium@gmail.com>
2025-02-02 14:30:18 +0900

    ioはio.tsへ・・・

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,7 @@
 import { Command } from "commander";
-import * as fs from 'fs';
-import { readAllStdin } from "./io";
 import { parseJSON } from "./parse";
 import { renderTSTrie } from "./render";
-
-async function readJSONsFromFiles(files: string[]) {
-  const jsons: any[] = [];
-  for (const filePath of files) {
-    try {
-      const content = fs.readFileSync(filePath, 'utf-8');
-      let json = JSON.parse(content);
-      if (!Array.isArray(json)) {
-        console.warn(`Warning: File ${filePath} is not an array, wrapping it in an array`);
-        json = [json];
-      }
-      jsons.push(...json);
-    } catch (err: any) {
-      console.error(`Error reading file ${filePath}: ${err.message}`);
-    }
-  }
-  return jsons;
-}
-
-async function readJSONsFromSTDIN() {
-  const input = await readAllStdin();
-  let json = JSON.parse(input);
-  if (!Array.isArray(json)) {
-    console.warn("Warning: Input is not an array, wrapping it in an array");
-    json = [json];
-  }
-  return json as any[];
-}
+import { readJSONsFromFiles, readJSONsFromSTDIN } from "./io";
 
 async function main() {
   const program = new Command();
--- src/io.ts
+++ src/io.ts
@@ -1,4 +1,6 @@
-export async function readAllStdin(): Promise<string> {
+import * as fs from 'fs';
+
+async function readAllStdin(): Promise<string> {
   process.stdin.setEncoding('utf-8');
   
   let data = '';
@@ -6,4 +8,32 @@ export async function readAllStdin(): Promise<string> {
     data += chunk;
   }
   return data;
+}
+
+export async function readJSONsFromFiles(files: string[]) {
+  const jsons: any[] = [];
+  for (const filePath of files) {
+    try {
+      const content = fs.readFileSync(filePath, 'utf-8');
+      let json = JSON.parse(content);
+      if (!Array.isArray(json)) {
+        console.warn(`Warning: File ${filePath} is not an array, wrapping it in an array`);
+        json = [json];
+      }
+      jsons.push(...json);
+    } catch (err: any) {
+      console.error(`Error reading file ${filePath}: ${err.message}`);
+    }
+  }
+  return jsons;
+}
+
+export async function readJSONsFromSTDIN() {
+  const input = await readAllStdin();
+  let json = JSON.parse(input);
+  if (!Array.isArray(json)) {
+    console.warn("Warning: Input is not an array, wrapping it in an array");
+    json = [json];
+  }
+  return json as any[];
 }
